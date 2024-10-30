16fa2f42ae5bcfc316bfbd11344b26d5a7922c56
DkFighT <116903571+DkFighT@users.noreply.github.com>
2024-10-30 03:52:51 +0300

    Only design the game

---
+++ app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/game')
+def game_screen():
+    return render_template('game.html', letters=5)
+
+
+def main():
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    main()
